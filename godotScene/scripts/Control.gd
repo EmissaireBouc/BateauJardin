047757d9add0de8f206a53d80320030627c706cb
nainkult@gmail.com <nainkult@gmail.com>
2020-07-10 10:40:21 +0200

    Amélioration du déplacement et réorganisation des scènes

--- godotScene/scripts/Control.gd
+++ godotScene/scripts/Control.gd
@@ -1,21 +1,22 @@
-extends Control
+extends Node2D
 
+# enum est une énumération des différentes actions que le personnage peut effectuer
 enum{IDLE, MOVE}
 
-#onready var nav2D : Navigation2D = $Navigation2D
-onready var Line2D : Line2D = $Line2D
-onready var Player : AnimatedSprite = $Player
-onready var nav2D = get_parent().get_node("Bateau/WalkArea")
+# Le mot clé "onready" permet d'assigner une valeur à une variable dès l'exécution du script. Remplace func _ready()
+# Navigation2D est un noeud qui permet le pathfinding depuis une aire de navigation (NavigationPolygon)
+onready var nav2D : Navigation2D = $Bateau/WalkArea
+# Line2D trace une ligne (débug)
+onready var Line2D : Line2D = $YSort/Line2D
+onready var Player : AnimatedSprite = $YSort/Player
 
-func _ready():
-	pass # Replace with function body.
 
+# _unhandled_input(event) récupère l'input souris DANS la scène active (normalement doit permettre de distinguer des inputs qui ont lieu dans le HUD) 
 func _unhandled_input(event):
 	if !Input.is_action_pressed("ui_left_mouse"):
 		return
+	# Création du chemin entre la position du personnage et le mouse(x,y)
 	var new_path = nav2D.get_simple_path(Player.get_global_position(), get_global_mouse_position())
-	print(get_global_mouse_position())
-	print(Player.get_global_position())
 	
 	Line2D.points = new_path
 	Player.path = new_path
--- godotScene/scripts/PlayerControl.gd
+++ godotScene/scripts/PlayerControl.gd
@@ -1,57 +1,54 @@
 extends AnimatedSprite
 
-export (int) var speed = 30
-var move_direction = 30
-var destination = Vector2()
 
-enum{IDLE, MOVE}
-var state
+export (int) var speed = 30
+var move_direction = 30 #prend un angle en degré
+var state #défini l'état du personnage (marche, idle, interact)
 var path : PoolVector2Array
 
-func _ready():
-	destination = position
+enum{IDLE, MOVE}
+
 
 func _process(delta):
-	var move_distance = speed * delta
 
 	match state:
 		IDLE:
-			Animation_Loop("IDLE")
+			animation_loop("IDLE")
 		MOVE:
+			var move_distance = speed * delta
 			move_along_path(move_distance)
-			Animation_Loop("WALK")
-
+			animation_loop("WALK")
 
 
 func move_along_path(distance):
+# Déplacement PJ sur le chemin tracé grâce à Navigation 2D
 	var starting_point : = position
 
 	for i in range(path.size()):
 		var distance_to_next : = starting_point.distance_to(path[0])
 		move_direction = rad2deg(path[0].angle_to_point(position))
-		
+
 		if (distance <= distance_to_next):
 			position = starting_point.linear_interpolate(path[0], distance / distance_to_next)
 			break
 		path.remove(0)
-		
+
 		if(path.size() == 0):
 			change_state(IDLE)
 
 
 func change_state(newstate):
+# Change l'état du PJ (marche, idle, plant...)
 	state = newstate
 	match state:
 		IDLE:
-			Animation_Loop("IDLE")
+			animation_loop("IDLE")
 		MOVE:
-			Animation_Loop("WALK")
+			animation_loop("WALK")
 
 
-
-
-func Animation_Loop(mode):
-
+func animation_loop(mode):
+# gestion de l'animation du PJ
 	var anim_direction = "S"
 	var anim_mode = mode
 	var animation
@@ -71,38 +68,3 @@ func Animation_Loop(mode):
 	self.play(animation) 
 
 
-#func _unhandled_input(event):
-#	#Si Clic de souris : target = Mouse(x,y)
-#	if event.is_action_pressed('ui_left_mouse'):
-#		moving = true
-#		destination = get_global_mouse_position()
-
-#func _walk(destination):
-#	if moving == false:
-#		speed = 30
-#
-#	movement = position.direction_to(destination) * speed
-#	move_direction = rad2deg(destination.angle_to_point(position))
-#
-#
-#	if position.distance_to(destination) > 5:
-#		movement = move_and_slide(movement)
-#	else: 
-#		moving = false
-#
-#	#Joue animation marche si velocity != 0
-#	if movement.length() > 0:
-#		movement = movement.normalized()*speed
-#		$AnimatedSprite.play()
-#	else:
-#		$AnimatedSprite.stop()
-
-#func _physics_process(delta):
-#	
-#	if cursorMode == 'walk':
-#		_walk(destination)
-			
-#	if moving == true:
-#		anim_mode = "Walk"
-#	elif moving == false:
-#		anim_mode = "Idle"
