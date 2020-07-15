b954f7616e91eb2d3e3312004e2b5b5ec97413e7
Paulette005 <paulette@zaclys.net>
2020-07-15 15:42:10 +0200

    @paulette005

--- godotScene/scripts/Control.gd
+++ godotScene/scripts/Control.gd
@@ -1,5 +1,15 @@
 extends Node2D
 
+# Load the custom images for the mouse cursor.
+var default = load("res://Assets/UI/Curseur/Curs__0.png")
+var planter = load("res://Assets/UI/Curseur/Curs__2.png")
+
+var cursor = "default"
+var jardin = []
+
+
+
+
 # enum est une énumération des différentes actions que le personnage peut effectuer
 enum{IDLE, MOVE}
 
@@ -15,12 +25,74 @@ onready var Player : AnimatedSprite = $YSort/Player
 func _unhandled_input(event):
 	if !Input.is_action_pressed("ui_left_mouse"):
 		return
-	# Création du chemin entre la position du personnage et le mouse(x,y)
-	var new_path = nav2D.get_simple_path(Player.get_global_position(), get_global_mouse_position())
-		
-	Line2D.points = new_path
-	Player.path = new_path
-	Player.change_state(MOVE)
 
-# Ouverture du menu contextuel	
+	if Input.is_action_pressed("ui_left_mouse"):
+		if cursor == "default":
+			# Création du chemin entre la position du personnage et le mouse(x,y)
+			var new_path = nav2D.get_simple_path(Player.get_global_position(), get_global_mouse_position())
+			Line2D.points = new_path
+			Player.path = new_path
+			Player.change_state(MOVE)
+
+
+# Ouverture du menu contextuel
+func _input(event):
+	if Input.is_action_pressed("ui_right_mouse"): 
+		if get_node_or_null("MenuInteractions") == null:
+			var MenuInteractions = load("res://Scenes/Menu_interactions.tscn").instance()
+			add_child(MenuInteractions)
+			cursor_mode("default")
+		else:
+			cursor_mode("planter")
+			$MenuInteractions.queue_free()
 		
+
+
+
+func _ready():
+	cursor_mode("default")
+	
+
+#Ne fonctionne pas pour le moment
+#func create_area2D():
+#	$Bateau/WalkArea/Area2D/CollisionPolygon2D.set_polygon($Bateau/WalkArea/NavigationPolygonInstance.navpoly.vertices)
+
+
+func cursor_mode(newMode):
+	cursor = newMode
+	if (newMode == "default"):
+		Input.set_custom_mouse_cursor(default)
+	if (newMode == "planter"):
+		Input.set_custom_mouse_cursor(planter)
+	
+	
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if (event is InputEventMouseButton && Input.is_action_pressed("ui_left_mouse") && cursor == "planter"):
+		var plante = preload("res://Scenes/Plante.tscn").instance()
+		$YSort.add_child(plante)
+		jardin.push_front(plante)
+		jardin[0].position = get_global_mouse_position()
+
+
+func _on_Jardin_mouse_entered():
+	if cursor == "planter":
+		Input.set_custom_mouse_cursor(planter)
+
+
+func _on_Jardin_mouse_exited():
+	if cursor == "planter":
+		var nplanter = load("res://Assets/UI/Curseur/Curs__3.png")
+		Input.set_custom_mouse_cursor(nplanter)
+
+
+func _on_Area2D_mouse_entered():
+	if cursor == "default":
+		Input.set_custom_mouse_cursor(default)
+
+
+func _on_Area2D_mouse_exited():
+	if cursor == "default":
+		var ndefault = load("res://Assets/UI/Curseur/Curs__1.png")
+		Input.set_custom_mouse_cursor(ndefault)
